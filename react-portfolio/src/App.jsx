1654d87cc68c45409b95912b152905defc5fde1a
Shankar <idofshankar@gmail.com>
2025-08-04 21:27:14 +0530

    Added

---
+++ react-portfolio/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="portfolio">
+      <h1>Hi, I'm Shankara Perumal Arunachalam</h1>
+      <h2>Frontend Developer (React, JavaScript, HTML, CSS)</h2>
+      <p>📍 India | 5 years experience | Aiming for ₹36L package</p>
+    </div>
+    </>
+  )
+}
+
+export default App
