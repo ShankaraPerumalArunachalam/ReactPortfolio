589fa6126391e259aaa257a76b54ed151a38ecac
Shankar <idofshankar@gmail.com>
2025-08-05 22:22:03 +0530

    vg

--- react-portfolio/vite.config.js
+++ react-portfolio/vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ReactPortfolio/', // your repo name
   plugins: [react()],
+  base: '/ReactPortfolio/', // 👈 use your exact repo name here
 })
